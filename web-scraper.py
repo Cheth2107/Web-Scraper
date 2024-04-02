b6494d37b846ad984300530d1e764ebe14f50852
Chethan K <143086835+Cheth2107@users.noreply.github.com>
2024-04-02 08:39:22 +0530

    Update web-scraper.py

--- web-scraper.py
+++ web-scraper.py
@@ -18,3 +18,24 @@ with open("output.html", "w", encoding="utf-8") as f:
     f.write(soup.prettify())
 
 print("Data has been saved to output.html")
+
+
+
+
+#################################################################
+
+https://github.com/CoreyMSchafer/code_snippets/tree/master/BeautifulSoup
+
+from bs4 import BeautifulSoup
+import requests
+
+source = requests.get('https://javascript.info/intro').text
+
+soup = BeautifulSoup(source,'lxml')
+
+content = soup.find("div",class_="content")
+
+main = content.get_text()
+
+print(main)
+
